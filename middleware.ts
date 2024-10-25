4532d69bd38a4025c58367be3ddff80a3b50082b
0xpo <65166393+0xpo@users.noreply.github.com>
2024-10-25 19:52:44 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,6 @@
 import { authMiddleware } from "@clerk/nextjs";
- 
+import { NextRequest, NextResponse } from "next/server";
+
 export default authMiddleware({
   publicRoutes: [
     '/',
@@ -14,8 +15,22 @@ export default authMiddleware({
     '/api/uploadthing'
   ]
 });
- 
+
+export function middleware(req: NextRequest) {
+  const res = NextResponse.next();
+
+  // Ajuster les attributs SameSite pour un cookie spécifique de Clerk
+  const cookie = req.cookies.get('__clerk_db_jwt');
+  if (cookie) {
+    res.cookies.set('__clerk_db_jwt', cookie.value, { // Utilisez cookie.value pour obtenir la chaîne
+      sameSite: 'none',
+      secure: true,
+    });
+  }
+
+  return res;
+}
+
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
- 
